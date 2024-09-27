f3f2dd0eaf8f7524ded9b18af48d58335beadac3
serudda <sergioruizdavila@gmail.com>
2024-09-27 17:45:13 -0500

    refactor: add new Guild rule

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -2,7 +2,9 @@ import { onMessage } from './events/onMessage';
 import { onReady } from './events/onReady';
 import { Client, GatewayIntentBits } from 'discord.js';
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
 
 client.once('ready', () => onReady(client));
 client.on('messageCreate', onMessage);
