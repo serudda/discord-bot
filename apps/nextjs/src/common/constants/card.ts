2e5d346b8f49bdee9a4ccbfcc4175afec70f3793
Samuel Llibre-Pillco <dev@zyruks.com>
2024-10-17 01:07:57 -0400

    feat(constants): ✨ add rarityOrder and asset paths to constants
    - Added rarityOrder constant to card.ts for sorting card rarities
    - Defined ASSETS_PATH and EMBLEMS_IMAGES_PATH in general.ts for asset management

---
+++ apps/nextjs/src/common/constants/card.ts
@@ -0,0 +1,3 @@
+import { Rarity } from '~/utils/api';
+
+export const rarityOrder: Array<Rarity> = [Rarity.LEGENDARY, Rarity.EPIC, Rarity.RARE, Rarity.UNCOMMON, Rarity.COMMON];
--- apps/nextjs/src/common/constants/index.ts
+++ apps/nextjs/src/common/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './errors';
+export * from './general';
 export * from './inputs';
