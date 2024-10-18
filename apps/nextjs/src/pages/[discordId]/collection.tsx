51ce20eb1ff16fc561a3c5ef25a67e9f53ef8edf
Samuel Llibre-Pillco <dev@zyruks.com>
2024-10-17 22:18:08 -0400

    refactor(collection): ♻️ update UserCollectionPage and improve sidebar and rarity handling

--- apps/nextjs/src/pages/[discordId]/collection.tsx
+++ apps/nextjs/src/pages/[discordId]/collection.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
-import type { CardCountMap } from '~/components/pages/user/collection';
-import { RaritySection, UserCardRarities, UserProfileImage } from '~/components/pages/user/collection';
 import { api } from '~/utils/api';
-import { rarityOrder } from '~/common/constants/card';
+import { rarityOrder } from '~/common';
+import { RaritySection, UserSidebar, type CardCountMap } from '~/components';
 import { useRouter } from 'next/router';
 
 const UserCollectionPage = () => {
@@ -12,7 +11,7 @@ const UserCollectionPage = () => {
   const userDiscordId = userData?.accounts.find((account) => account.provider === 'discord')?.providerAccountId;
   const { data: collectionData } = api.card.getCollection.useQuery({ discordId: userDiscordId as string });
   const { data: allCardsData } = api.card.getAllCards.useQuery({});
-  const [showUnknown, setShowUnknown] = useState(false);
+  const [showUnownedCards, setShowUnownedCards] = useState(false);
 
   const ownedCardMap = new Map<string, CardCountMap>();
   collectionData?.result?.collection?.forEach((cardCountMap) => {
@@ -22,20 +21,39 @@ const UserCollectionPage = () => {
   const ownedCardIds = new Set(collectionData?.result?.collection?.map(({ card }) => card.id));
   const ownedCardCount = ownedCardIds.size;
 
+  const ownedCountsPerRarity: Record<string, number> = {};
+  let ownedFoilCount = 0;
+
+  rarityOrder.forEach((rarity) => {
+    ownedCountsPerRarity[rarity] = 0;
+  });
+
+  collectionData?.result?.collection?.forEach(({ card, isFoil }) => {
+    const rarity = card.rarity;
+    if (rarity && !isFoil) ownedCountsPerRarity[rarity] = (ownedCountsPerRarity[rarity] ?? 0) + 1;
+
+    if (isFoil) ownedFoilCount += 1;
+  });
+
   return (
     <>
       <main className="bg-neutral-900">
         <div className="container mx-auto grid-cols-[0.3fr,1fr] grid">
-          <aside className="sticky top-10 h-fit text-white">
-            <UserProfileImage userImage={userData?.image as string} ownedCardCount={ownedCardCount} />
-            <UserCardRarities showUnknown={showUnknown} setShowUnknown={setShowUnknown} />
-          </aside>
+          <UserSidebar
+            userImage={userData?.image as string}
+            ownedCardCount={ownedCardCount}
+            showUnownedCards={showUnownedCards}
+            setShowUnownedCards={setShowUnownedCards}
+            ownedCountsPerRarity={ownedCountsPerRarity}
+            ownedFoilCount={ownedFoilCount}
+          />
+
           <div className="container mx-auto p-4">
             {rarityOrder.map((rarity) => (
               <RaritySection
                 key={rarity}
                 rarity={rarity}
-                showUnknown={showUnknown}
+                shownUnownedCards={showUnownedCards}
                 groupByRarity={(rarity) => allCardsData?.result?.cards?.filter((card) => card.rarity === rarity) ?? []}
                 ownedCardIds={ownedCardIds}
                 ownedCardMap={ownedCardMap}
