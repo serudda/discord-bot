96b7e4f7ab2941b4d884cdbefbdaf12749f9d825
Samuel Llibre-Pillco <dev@zyruks.com>
2024-10-23 21:53:38 -0400

    feat(icons): ✨ add icon.tsx for managing SVG icons with catalog enum

--- apps/nextjs/src/components/index.ts
+++ apps/nextjs/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './button/button';
 export * from './dialog/dialog';
+export * from './icon/icon';
 export * from './pages';
 export * from './select/select';
 export * from './tooltip/tooltip';
